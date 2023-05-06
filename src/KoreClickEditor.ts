f4dc589b388ef592d281d42ccfba004e66661be1
lwohtsu <ohtsu-y1@libroworks.co.jp>
2023-05-06 18:44:33 +0900

    WIP:SVGを表示

--- src/KoreClickEditor.ts
+++ src/KoreClickEditor.ts
@@ -12,6 +12,8 @@ export class KoreClickEditorProvider implements vscode.CustomTextEditorProvider 
 
   private static readonly scratchCharacters = ["😸", "😹", "😺", "😻", "😼", "😽", "😾", "🙀", "😿", "🐱"];
 
+  private static baseUrl = "http://localhost:8800/";
+
   constructor(private readonly context: vscode.ExtensionContext) {}
 
   /**
@@ -20,9 +22,13 @@ export class KoreClickEditorProvider implements vscode.CustomTextEditorProvider 
    *
    */
   public async resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, _token: vscode.CancellationToken): Promise<void> {
+    // const workDir = vscode.workspace.workspaceFolders[0].uri;
+    // console.log("**workspace is " + workDir.toString());
+    // console.log("**workspace is " + vscode.Uri.joinPath(this.context.extensionUri, "media").toString());
     // Setup initial content for the webview
     webviewPanel.webview.options = {
       enableScripts: true,
+      // localResourceRoots: [workDir, vscode.Uri.joinPath(this.context.extensionUri, "media")],
     };
     webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
 
@@ -30,6 +36,8 @@ export class KoreClickEditorProvider implements vscode.CustomTextEditorProvider 
       webviewPanel.webview.postMessage({
         type: "update",
         text: document.getText(),
+        svguri: `${KoreClickEditorProvider.baseUrl}test.svg`,
+        // document.uri.toString(),
       });
     }
 
@@ -73,43 +81,74 @@ export class KoreClickEditorProvider implements vscode.CustomTextEditorProvider 
    */
   private getHtmlForWebview(webview: vscode.Webview): string {
     // Local path to script and css for the webview
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "catScratch.js"));
+    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "koreclick.js"));
 
     const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "reset.css"));
 
     const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "vscode.css"));
 
-    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "catScratch.css"));
+    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "media", "koreclick_ui.css"));
 
     // Use a nonce to whitelist which scripts can be run
     const nonce = getNonce();
 
     return /* html */ `
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<!--
-				Use a content security policy to only allow loading images from https or from our extension directory,
-				and only allow scripts that have a specific nonce.
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="${styleResetUri}" rel="stylesheet" />
-				<link href="${styleVSCodeUri}" rel="stylesheet" />
-				<link href="${styleMainUri}" rel="stylesheet" />
-				<title>Cat Scratch</title>
-			</head>
-			<body>
-				<div class="notes">
-					<div class="add-button">
-						<button>Scratch!</button>
-					</div>
-				</div>
-				
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
+      <!DOCTYPE html>
+      <html lang="ja">
+      <head>
+        <meta charset="UTF-8">
+        <!--
+        Use a content security policy to only allow loading images from https or from our extension directory,
+        and only allow scripts that have a specific nonce.
+        -->
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} data:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="${styleResetUri}" rel="stylesheet" />
+        <link href="${styleVSCodeUri}" rel="stylesheet" />
+        <link href="${styleMainUri}" rel="stylesheet" />
+        <title>kore wo click</title>
+      </head>
+      <body>
+        <div class="kc-container">
+          <div class="notes">
+            <img id="img_svg" />
+          </div>
+          <select id="kcins-elemlist" size="20" class="kc-elemlist">
+            <option>svg width="100" height="40"</option>
+          </select>
+          <div class="kc-console">
+            <div class="kc-inspector">
+              <label id="kcins-type">type</label>
+              <label>class</label>
+              <input id="kcins-class" type="text" placeholder="class name"/>
+              <label>x1</label>
+              <input id="kcins-x1" type="number" step="0.1" placeholder="0"/>
+              <label>y1</label>
+              <input id="kcins-y1" type="number" step="0.1" placeholder="0"/>
+              <label>x2</label>
+              <input id="kcins-x2" type="number" step="0.1" placeholder="0"/>
+              <label>y2</label>
+              <input id="kcins-y2" type="number" step="0.1" placeholder="0"/>
+              <label>w</label>
+              <input id="kcins-w" type="number" step="0.1" placeholder="0"/>
+              <label>h</label>
+              <input id="kcins-h" type="number" step="0.1" placeholder="0"/>
+              <label>imgsrc</label>
+              <input id="kcins-imgpath" type="file" placeholder="class name"/>
+            </div>
+            <div class="kc-toolbox">
+              <button>LN</button>
+              <button>PL</button>
+              <button>BX</button>
+              <button>T1</button>
+              <button>T2</button>
+            </div>
+          </div>
+        </div>
+    
+        <script nonce="${nonce}" src="${scriptUri}"></script>
+      </body>
+      </html>`;
   }
 
   /**
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,8 @@
 import * as vscode from "vscode";
 import { KoreClickEditorProvider } from "./KoreClickEditor";
+import * as http from "http";
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "koreclick-svgeditor" is now active!');
@@ -10,6 +13,75 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 
   context.subscriptions.push(KoreClickEditorProvider.register(context));
+
+  // Webサーバ起動
+  // const targetPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  // if (!targetPath) {
+  //   vscode.window.showWarningMessage("[Live Server] フォルダを開いた状態で実行してください");
+  //   return;
+  // }
+
+  // type Request = http.IncomingMessage | { url: string };
+  // type Response = http.ServerResponse;
+
+  // const hostname = "127.0.0.1";
+  // const port = 8800;
+  // const dirPublic = targetPath;
+  // const indexFile = "index.html";
+
+  // const service = function (req: Request, res: Response) {
+  //   const pathname = req.url ?? "";
+  //   const root = path.resolve(dirPublic);
+  //   const file = path.join(root, pathname);
+
+  //   const extname = String(path.extname(pathname)).toLowerCase();
+  //   const mimeTypes = {
+  //     ".html": "text/html",
+  //     ".js": "text/javascript",
+  //     ".css": "text/css",
+  //     ".json": "application/json",
+  //     ".png": "image/png",
+  //     ".jpg": "image/jpg",
+  //     ".gif": "image/gif",
+  //     ".svg": "image/svg+xml",
+  //     ".wav": "audio/wav",
+  //     ".mp4": "video/mp4",
+  //     ".woff": "application/font-woff",
+  //     ".ttf": "application/font-ttf",
+  //     ".eot": "application/vnd.ms-fontobject",
+  //     ".otf": "application/font-otf",
+  //     ".wasm": "application/wasm",
+  //   };
+
+  //   const contentType = mimeTypes[extname] || "application/octet-stream";
+
+  //   fs.stat(file, (err, stat) => {
+  //     if (err) {
+  //       console.log("err", err);
+  //       res.write("err");
+  //       res.end();
+  //       return;
+  //     }
+
+  //     if (stat.isDirectory()) {
+  //       service({ url: `${req.url}/${indexFile}` }, res);
+  //     }
+
+  //     if (stat.isFile()) {
+  //       const stream = fs.createReadStream(file);
+  //       stream.pipe(res);
+  //       res.writeHead(200, { "Content-Type": contentType });
+  //       console.log(req.url);
+  //     }
+  //   });
+  // };
+
+  // const listener = function (req: Request, res: Response) {
+  //   service(req, res);
+  // };
+
+  // const server = http.createServer(listener);
+  // server.listen(port, hostname);
 }
 
 // this method is called when your extension is deactivated
